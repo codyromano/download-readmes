ee549055892be8899bda0b77c67b613214eea4d2
Cody Romano <codyromano@gmail.com>
2017-09-04 17:07:47 -0700

    Update file name

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+from time import gmtime, strftime
+from datetime import datetime
+
+today = datetime.today()
+result_file_name = "result-" + today.strftime("%a-%m-%d-%I%M%p") + ".csv"
+
 config = {
     "FOLDER_NAME_MAIN": "readme-data",
-    "FILE_NAME_README_CSV": "readme-data.csv",
+    "FILE_NAME_README_CSV": result_file_name,
     "PATH_CSV_SOURCE": "readme-data/users_projects.csv"
 }
