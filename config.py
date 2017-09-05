f2cc14e56d79db46671bcb795433bede29955b89
Cody Romano <codyromano@gmail.com>
2017-09-04 17:17:41 -0700

    Split user_projects source data into chunks

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ result_file_name = "result-" + today.strftime("%a-%m-%d-%I%M%p") + ".csv"
 config = {
     "FOLDER_NAME_MAIN": "readme-data",
     "FILE_NAME_README_CSV": result_file_name,
-    "PATH_CSV_SOURCE": "readme-data/users_projects.csv"
+    "PATH_CSV_SOURCE": "readme-data/user_projects_1.csv"
 }
